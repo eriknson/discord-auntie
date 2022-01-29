f127c19c82a0c40c955c294da1fb396bb1d183d2
eriknson <eriks@mail.se>
2022-01-29 17:38:55 +0100

    work in progress

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,21 @@ config()
 if (typeof process.env.TOKEN !== 'string') {
   throw new Error('Missing process.env.TOKEN')
 }
+if (typeof process.env.CRON_CHANNEL_ID !== 'string') {
+  throw new Error('Missing process.env.CRON_CHANNEL_ID')
+}
 
-const getEmbed = async () => {
+const getEmbed = async (collecting: boolean = true) => {
   const price = await getStats()
 
   const embed = new MessageEmbed()
-    .setColor('#FFFFFF')
-    .setTitle('Gm Matos! Are you feeling bullish today?')
+    .setColor(`${collecting ? '#00b84d' : '#c5c5c5'}`)
+    .setTitle(`Gm! Are you feeling bullish?`)
     .setDescription(
-      `React with your prediction — 🐮 or 🐻 — and we'll see in 24h if you were right.`
+      `React with your prediction — 🐮 or 🐻 — and we'll circle back in 24h to see if your prediction matches the market.`
     )
     .addFields({
-      name: `${price.bullish ? '📈' : '📉'} **Ethereum market price**`,
+      name: `${price.bullish ? '📈' : '📉'} ** Ethereum market price**`,
       value: `${price.value} ${getHighlighted(price.oneDayDiff)} `,
     })
 
@@ -30,61 +33,91 @@ const getHighlighted = (value = '') => (value ? ' `' + value + '`' : '')
 
 const start = async () => {
   const client = await getClient()
+  const cronChannel = await client.channels.cache.get(
+    process.env.CRON_CHANNEL_ID
+  )
+
+  try {
+    console.log('Debug msg triggered!')
+    if (cronChannel) {
+      const embed = await getEmbed(true)
+      // create prediction entity with user token current market price and embedId
+      ;(cronChannel as any).send({ embeds: [embed] })
+    }
+  } catch (e) {
+    console.error(e)
+  }
 
   client.on('messageCreate', async (msg) => {
-    if (
-      msg.content.toLowerCase().includes('gm') &&
-      msg.author.id != process.env.BOT_ID
-    ) {
+    // Coffee reaction to gm's
+    if (msg.content.toLowerCase().includes('gm') && !msg.author.bot) {
       msg.react('☕')
     }
-    if (
-      msg.channelId === process.env.CRON_CHANNEL_ID &&
-      msg.author.id == process.env.BOT_ID
-    ) {
-      // Initialize bullish / bearish reactions
-      msg.react('🐻')
+
+    // Bot's own gm message
+    if (msg.channelId === process.env.CRON_CHANNEL_ID && msg.author.bot) {
       msg.react('🐮')
+      msg.react('🐻')
 
-      // WIP: Reaction collector
       const filter = (reaction: MessageReaction, user: User) =>
-        user.id != process.env.BOT_ID
+        !user.bot && ['🐮', '🐻'].includes(reaction.emoji.name)
 
+      let options = { time: 7200 /* 0000 */ }
       const collector = msg.createReactionCollector({
         filter,
-        time: 1000 * 5,
+        ...options,
       })
 
-      collector.on('collect', (reaction) => {
-        console.log(reaction.emoji.name)
+      collector.on('collect', (_reaction, _user) => {
+        console.log(_reaction.emoji.name, _user.username)
+
+        // Enforce only one reaction per user
+        _reaction.message.reactions.cache.map((x) => {
+          if (
+            x.emoji.name != _reaction.emoji.name &&
+            x.users.cache.has(_user.id)
+          ) {
+            x.users.remove(_user.id)
+          }
+        })
+      })
+
+      collector.on('end', async (collected) => {
+        await getEmbed(false).then((embed) => msg.edit({ embeds: [embed] }))
+        const reactions = collected.map((_reaction) => ({
+          reaction: _reaction.emoji.name,
+          users: _reaction.users.cache
+            .filter((user) => !user.bot)
+            .map((user) => user.username),
+        }))
+
+        console.log(reactions)
       })
     }
   })
 
-  if (typeof process.env.CRON_CHANNEL_ID === 'string') {
-    const channel = await client.channels.cache.get(process.env.CRON_CHANNEL_ID)
-    const job = new CronJob(
-      '1 * * * * *',
-      async () => {
-        try {
-          console.log('Cron job triggered!')
-          if (channel) {
-            const embed = await getEmbed()
-            ;(channel as any).send({ embeds: [embed] })
-          }
-        } catch (e) {
-          console.error(e)
+  // cronjob to send gm
+  const channel = await client.channels.cache.get(process.env.CRON_CHANNEL_ID)
+  const job = new CronJob(
+    '1 * * * * *',
+    async () => {
+      try {
+        console.log('Cron job triggered!')
+        if (channel) {
+          const embed = await getEmbed(true)
+          // create prediction entity with user token current market price and embedId
+          ;(channel as any).send({ embeds: [embed] })
         }
-      },
-      null,
-      true,
-      'Europe/Berlin'
-    )
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    null,
+    true,
+    'Europe/Berlin'
+  )
 
-    job.start()
-  } else {
-    console.error('Missing typeof process.env.CRON_CHANNEL_ID')
-  }
+  job.start()
 }
 
 start()
